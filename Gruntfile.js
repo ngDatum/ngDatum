7c1d9e8cb31b57a948d4311b342247b768563602
mcpike <patrick.mcpike@gmail.com>
2015-01-12 15:52:23 -0800

    setup jshint task in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,24 @@
 module.exports = function(grunt) {
 
+  // Load multiple grunt tasks using globbing patterns
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-
+    concat: {
+      test: {
+        src: ['src/chart.js','src/**/*.js'],
+        dest: 'dist/test.js'
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js']
+    }
   });
 
 
 
+  // grunt.registerTask('concat-test', [concat:test]);
+  // simple to task to test that grunt is working  
   grunt.registerTask('test', function(){
     console.log('Grunt is working');
   });
